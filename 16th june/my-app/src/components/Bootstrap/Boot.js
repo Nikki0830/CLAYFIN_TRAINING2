259525ae25566199eaf8b7054aec6f323bfe570b
Davis Kumar Nayak <you@example.com>
2022-06-27 11:36:29 +0530

    bootstrap

--- 16th june/my-app/src/components/Bootstrap/Boot.js
+++ 16th june/my-app/src/components/Bootstrap/Boot.js
@@ -1,19 +1,52 @@
 import React from "react";
-import  "../../../node_modules/bootstrap/dist/css/bootstrap.min.css"
+import "../../../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import Button from 'react-bootstrap/Button';
 
 export default function Boot() {
   return (
-    <div>
-      <div className="container">
-        <div
-          className="row"
-        //   style={{ border: "1px solid red", height: "20px", height: "20px" }}
-        >
-          <div className="col-lg-4" style={{ border: "1px solid red" }}>column</div>
-          <div className="col-lg-4" style={{ border: "1px solid red" }}>column</div>
-          <div className="col-sm-8" style={{ border: "1px solid red" }}>column</div>
-        </div>
+    <>
+    <Button>Text button</Button>
+      {/* <div> */}
+      {/* <div className="container">
+          <div
+            className="row"
+        
+          >
+            <div className="col-lg-4" style={{ border: "1px solid red" }}>
+              column
+            </div>
+            <div className="col-lg-4" style={{ border: "1px solid red" }}>
+              column
+            </div>
+            <div className="col-sm-8" style={{ border: "1px solid red" }}>
+              column
+            </div> */}
+
+      <div class="mb-3">
+        <label for="exampleFormControlInput1" class="form-label">
+          Email address
+        </label>
+        <input
+          type="email"
+          class="form-control"
+          id="exampleFormControlInput1"
+          placeholder="name@example.com"
+        />
       </div>
-    </div>
+      <div class="mb-3">
+        <label for="exampleFormControlTextarea1" class="form-label">
+          Example textarea
+        </label>
+        <textarea
+          class="form-control"
+          id="exampleFormControlTextarea1"
+          rows="3"
+        ></textarea>
+      </div>
+      {/* </div>
+        </div>
+      </div> */}
+
+    </>
   );
 }
